92daa73429a4c3263d302fad859ff52a321893a0
bya2 <byaa1972@gmail.com>
2022-01-20 14:51:55 +0900

    update

--- controller/db.js
+++ controller/db.js
@@ -1,7 +1,6 @@
 const oracledb = require("oracledb");
 const config__oracle_info = require("../config/oracle");
 
-<<<<<<< HEAD
 const XE_COMMON_TABLE = process.env.XE_COMMON_TABLE;
 const SECTION_ID = process.env.SECTION_ID;
 const FACTOR_TYPE = process.env.FACTOR_TYPE;
@@ -11,21 +10,6 @@ const arr_columns__section_sensor_types =
   str_columns__section_sensor_types.split(",");
 const arr_column_params = arr_columns__section_sensor_types.map((el, i) => {
   return `:VAL${++i}`;
-=======
-const COMMON_XE_TABLE = "general";
-const arr_columns__common_sensor_types = [
-  "vibration_1_val",
-  "noise_1_val",
-];
-const arr_columns__sensor_type__tunnel = [
-  ...arr_columns__common_sensor_types,
-  "accel_1_val",
-  "accel_2_val",
-  "accel_3_val",
-];
-const arr_column_params = arr_columns__sensor_type__tunnel.map((el, i) => {
-  return `VAL${i}`;
->>>>>>> 91f78ba2ebcd920d4e3b058cbc46cd5db223471a
 });
 const obj_option__bind_defs = arr_column_params.reduce(
   (obj, t) => ((obj[t] = { type: oracledb.DB_TYPE_NUMBER }), obj),
